79217ac800d4cb6f71ed0393e21760e6e2307c14
Miguel Ben <itsmigben@gmail.com>
2019-12-10 21:29:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -15,7 +15,7 @@ def directors_totals(nds)
 #   "2nd Director's Name" => 1234577890,
 #   "3rd Director's Name" => 1234709136,
 #   ...
-# }
+#   }
   
   # pp nds - needed to verify that we are working with the same NDS
   
